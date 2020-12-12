b9b50c7e4b5a23c14c75e3ddfcf9b2147a0325a0
bin.wu <jssnwubin@gmail.com>
2020-12-12 20:48:39 +0800

    Add PDFium
    Mac Catalyst is not supported now.

--- Lykoi/ViewModel/PDFDocViewModel.swift
+++ Lykoi/ViewModel/PDFDocViewModel.swift
@@ -16,5 +16,9 @@ class PDFDocViewModel: ObservableObject {
         DDLogInfo("Info")
         DDLogWarn("Warn")
         DDLogError("Error")
+        
+        var config = FPDF_LIBRARY_CONFIG(version: 2, m_pUserFontPaths: nil, m_pIsolate: nil, m_v8EmbedderSlot: 0, m_pPlatform: nil)
+        FPDF_InitLibraryWithConfig(&config)
+        FPDF_DestroyLibrary()
     }
 }
